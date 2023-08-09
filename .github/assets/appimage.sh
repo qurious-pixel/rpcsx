e224bd3c4540279632c8c6f1835ba658761ed029
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-08-09 09:16:30 -0700

    Update appimage.sh

--- .github/assets/appimage.sh
+++ .github/assets/appimage.sh
@@ -6,8 +6,8 @@ cp build/bin AppDir/usr/
 chmod +x AppDir/usr/bin
 ln -sr AppDir/usr/bin/Mesen AppDir/AppRun
 
-cp .github/assets/${BINNAME}.png AppDir/
-cp .github/assets/${BINNAME}.desktop AppDir/
+cp ci/.github/assets/${BINNAME}.png AppDir/
+cp ci/.github/assets/${BINNAME}.desktop AppDir/
 mkdir -p AppDir/usr/share/applications && cp ./AppDir/${BINNAME}.desktop ./AppDir/usr/share/applications
 mkdir -p AppDir/usr/share/icons && cp ./AppDir/${BINNAME}.png ./AppDir/usr/share/icons
 mkdir -p AppDir/usr/share/icons/hicolor/512x512/apps && cp ./AppDir/${BINNAME}.png ./AppDir/usr/share/icons/hicolor/512x512/apps
