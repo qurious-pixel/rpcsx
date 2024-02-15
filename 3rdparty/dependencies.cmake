bd55716cebc6459f39c40a2eef767093e2a47e1a
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-15 04:04:17 -0800

    Create dependencies.cmake

---
+++ 3rdparty/dependencies.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
+
+FetchContent_Declare(
+  VulkanHeaders
+  GIT_REPOSITORY "https://github.com/KhronosGroup/Vulkan-Headers.git"
+  GIT_TAG "v1.3.264"
+  OVERRIDE_FIND_PACKAGE)
+FetchContent_MakeAvailable(VulkanHeaders)
+
+FetchContent_Declare(spirv-cross
+	GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Cross.git
+    GIT_TAG sdk-1.3.261.1
+    GIT_PROGRESS ON)
+FetchContent_MakeAvailable(spirv-cross)
+include_directories(${spirv-cross_SOURCE_DIR})
